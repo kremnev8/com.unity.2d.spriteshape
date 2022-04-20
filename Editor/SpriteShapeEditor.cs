b63ecb663e4bad91e0126348da1c854d5d8765f5
kremnev8 <kremnev8super@yandex.ru>
2022-04-20 18:23:47 +0300

    Fix slider issue

--- Editor/SpriteShapeEditor.cs
+++ Editor/SpriteShapeEditor.cs
@@ -275,7 +275,7 @@ namespace UnityEditor.U2D
             EditorGUILayout.Space();
             DrawHeader(Contents.fillLabel);
             EditorGUILayout.PropertyField(m_FillTextureProp, Contents.fillTextureLabel);
-            EditorGUILayout.Slider(m_FillOffsetProp, -4, 1.5f, Contents.fillScaleLabel);
+            EditorGUILayout.Slider(m_FillOffsetProp, -4, 0.5f, Contents.fillScaleLabel);
 
 
             if (m_FillTextureProp.objectReferenceValue != null)
